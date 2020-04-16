5535254bd07caae77044b2d1749d4781d66f9460
John Carter <jc@john.carter.eu>
2020-04-16 17:06:59 +0200

    Initial Commit

---
+++ Cumulocity Client LibraryTests/Cumulocity_Client_LibraryTests.swift
@@ -0,0 +1,34 @@
+//
+//  Cumulocity_Client_LibraryTests.swift
+//  Cumulocity Client LibraryTests
+//
+//  Created by John Carter on 16/04/2020.
+//  Copyright © 2020 John Carter. All rights reserved.
+//
+
+import XCTest
+@testable import Cumulocity_Client_Library
+
+class Cumulocity_Client_LibraryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
