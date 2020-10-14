d8c4cb6e8e56daeddc42527cc9bda01bdb3c18a0
John Carter <john.carter@softwareag.com>
2020-10-14 12:38:56 +0200

    fixes for package management

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "Cumulocity Client Library",
-			targets: ["c8y-lib"])
+			targets: ["Cumulocity Client Library"])
 	],
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
@@ -19,7 +19,8 @@ let package = Package(
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
-			name: "c8y-lib",
+			name: "Cumulocity Client Library",
+			path: "Sources/c8y-lib",
 			exclude: ["docs"]
 		)
 	]
